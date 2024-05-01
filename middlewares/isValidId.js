1d9e6f9013bd3f0cccdee2e92a6690e232418822
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-05-01 10:05:35 +0300

    init

---
+++ middlewares/isValidId.js
@@ -0,0 +1,14 @@
+import { isValidObjectId } from "mongoose";
+import HttpError from "../helpers/HttpError.js";
+
+const isValidId = (req, res, next) => {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+        return next(HttpError(404, `${id} is not valid`));
+    }
+
+    next();
+};
+
+export default isValidId;
