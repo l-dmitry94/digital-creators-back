cbf4a3e3ddef361dddb07677fe9fbf90f97d32b7
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-05-06 16:43:08 +0300

    change editProfile

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -15,7 +15,7 @@ authRouter.post('/verify', validateBody(userEmailSchema), authControllers.resend
 
 authRouter.post('/signin', validateBody(userSigninSchema), authControllers.signin);
 
-authRouter.patch('/editprofile', authenticate, upload.single('avatar'), authControllers.updateAvatar);
+authRouter.patch('/editProfile', authenticate, upload.single('avatar'), authControllers.updateAvatar);
 
 authRouter.get('/current', authenticate, authControllers.getCurrent);
 
