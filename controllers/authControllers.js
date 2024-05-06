29ed973f056899a81efb79a506d57821d5d1082e
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-05-06 13:07:47 +0300

    added token for registration

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -20,13 +20,18 @@ const signup = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
-    const verificationToken = nanoid();
-    const newUser = await authServices.signup({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+    // const verificationToken = nanoid();
 
-    const verifyEmail = createVerifyEmail(email, verificationToken);
-    await sendEmail(verifyEmail);
+    const payload = { email };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
+
+    const newUser = await authServices.signup({ ...req.body, password: hashPassword, avatarURL, token });
+
+    // const verifyEmail = createVerifyEmail(email, verificationToken);
+    // await sendEmail(verifyEmail);
 
     res.status(201).json({
+        token,
         user: {
             username: newUser.username,
             email: newUser.email,
@@ -71,7 +76,7 @@ const signin = async (req, res) => {
     const { email, password } = req.body;
     const user = await authServices.findUser({ email });
     if (!user) throw HttpError(401, 'Email or password is wrong');
-    if (!user.verify) throw HttpError(401, 'Email not verify');
+    // if (!user.verify) throw HttpError(401, 'Email not verify');
 
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) throw HttpError(401, 'Email or password is wrong');
