99946926bd93177c199b9059b21f79bcf8417851
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-05-08 21:17:38 +0300

    add coment invalid for eddit profile

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -3,9 +3,9 @@ import Joi from 'joi';
 import { usernameRegexp, emailRegexp, passwordRegexp } from '../constants/userConstants.js';
 
 export const userSignupSchema = Joi.object({
-    username: Joi.string().pattern(usernameRegexp).required(),
-    email: Joi.string().pattern(emailRegexp).required(),
-    password: Joi.string().pattern(passwordRegexp).required(),
+    username: Joi.string().pattern(usernameRegexp).message('username invalid').required(),
+    email: Joi.string().pattern(emailRegexp).message('email invalid').required(),
+    password: Joi.string().pattern(passwordRegexp).message('password invalid').required(),
 });
 
 export const userSigninSchema = Joi.object({
