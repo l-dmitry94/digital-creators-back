27214e11f36aa25036264882d5ea6d8d2c708619
BestStepInUa <beststep.in.ua@gmail.com>
2024-05-14 00:46:05 +0300

    DONE

--- services/cardServices.js
+++ services/cardServices.js
@@ -6,6 +6,8 @@ export const updateCardByFilter = (filter, data) => Card.findOneAndUpdate(filter
 
 export const removeCardByFilter = filter => Card.findOneAndDelete(filter);
 
+export const removeAllCardsInBoards = filter => Card.deleteMany(filter);
+
 export const getAllCards = filter => Card.find(filter);
 
 export const getCardByFilter = filter => Card.findOne(filter);
