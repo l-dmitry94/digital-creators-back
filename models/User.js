69091e2640fc22dfd5ac3fb1f20c78a8b772b9bf
BestStepInUa <beststep.in.ua@gmail.com>
2024-05-15 15:33:47 +0300

    DONE

--- models/User.js
+++ models/User.js
@@ -31,6 +31,10 @@ const userSchema = new Schema(
             type: String,
             default: null,
         },
+        thema: {
+            type: String,
+            default: 'dark',
+        }
     },
     { versionKey: false, timestamps: true }
 );
